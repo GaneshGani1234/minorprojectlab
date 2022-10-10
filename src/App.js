2f0939522c0b5e458f5f2faf86c3a3ec5c832254
sid1523 <siddarthkassyap69@gmail.com>
2022-10-10 14:38:01 +0530

    1st git

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionComponent from './Components/FunctionComponent';
+
+function App() {
+  return (
+    <div>
+    <h1>Hello World</h1>
+    <FunctionComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/FunctionComponent.js
@@ -0,0 +1,7 @@
+function functcomp()
+{
+    return (
+        <h1>This is function Component</h1>
+    )
+}
+export default functcomp;
